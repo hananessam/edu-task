f105a410e9ed442e374c7c57d070df771632bffa
hananessam <hanan.essam228@gmail.com>
2023-10-12 22:58:51 +0300

    handel transactions filter

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -8,9 +8,28 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    public function transactions()
+    public function transactions(Request $request)
     {
-        $transactions = Transaction::with(['user'])->get();
+        $transactions = Transaction::with(['user'])->where(function ($query) use ($request) {
+            if ($request->exists('statusCode')) {
+                $query->where('statusCode', $request->statusCode);
+            }
+            if ($request->exists('currency')) {
+                $query->where('currency', $request->currency);
+            }
+            if ($request->exists('amountFrom')) {
+                $query->where('paidAmount', '>=', $request->amountFrom);
+            }
+            if ($request->exists('amountTo')) {
+                $query->where('paidAmount', '<=', $request->amountTo);
+            }
+            if ($request->exists('dateFrom')) {
+                $query->whereDate('paymentDate', '>=', $request->dateFrom);
+            }
+            if ($request->exists('dateTo')) {
+                $query->whereDate('paymentDate', '<=', $request->dateTo);
+            }
+        })->get();
 
         return response()->json([
             'transactions' => $transactions,
